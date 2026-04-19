3ba976f7f5719ff5aa7c792967d547019461490e
Semen Boldyrev <233458958+SemenBoldyrev@users.noreply.github.com>
2026-04-19 13:08:13 +0300

    Taken my old state machine

---
+++ Script/StateMachineScript/StateMachine.cs
@@ -0,0 +1,79 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HV.Scripts.StateMachine.Example
+{
+    public partial class StateMachine: Node
+    {
+
+        private Dictionary<string, StateNode> playerStates = new();
+        [Export] private StateNode initialState;
+        private StateNode nextState;
+        private StateNode curState;
+
+        public string CurStateName => curState.Name;
+
+        //INIT
+        public override void _Ready()
+        {
+            SetStates();
+            curState = initialState;
+            curState.Enter();
+        }
+
+        //CONTROLL
+        public override void _Input(InputEvent @event)
+        {
+            //not everyone need this
+            if (curState != null) { curState.Input(@event); }
+        }
+
+        public override void _Process(double delta)
+        {
+            if (curState != null) { curState.Update(delta); }
+        }
+
+        public override void _PhysicsProcess(double delta)
+        {
+            if (curState != null) { curState.PhysicsUpdate(delta); }
+        }
+
+        //DEFINE
+        private void SetStates()
+        {
+            foreach (StateNode state in this.GetChildren())
+            {
+                if (state is StateNode)
+                {
+                    playerStates.Add(state.Name.ToString().ToLower(), state);
+                    state.transition += StateTransitionExit;
+                    state.finished += StateTransitionFinish;
+                }
+            }
+        }
+
+        private void StateTransitionExit(object sender, string name)
+        {
+            //GD.Print(playerStates.Count);
+            name = name.ToLower();
+            if (!playerStates.ContainsKey(name)) { return; }
+
+            StateNode newState = playerStates[name];
+            if (curState == newState) { return; }
+
+            nextState = newState;
+            curState.Exit();
+        }
+
+        private void StateTransitionFinish()
+        {
+            curState = nextState;
+            nextState = null;
+            curState.Enter();
+        }
+    }
+}
